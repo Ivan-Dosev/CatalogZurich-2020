affd509f7fd4aec9806438da247fbaf31ca3f3fd
Ivan <biobeehive.com>
2020-09-13 06:44:49 +0300

    fierst view animation

--- CatalogZurich/ContentView.swift
+++ CatalogZurich/ContentView.swift
@@ -9,8 +9,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State  var sizeFond : CGFloat = UIScreen.main.bounds.width / 1.2
+
+    @State var isShow : Bool = false
+
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Group{
+                Text("ZURICH")
+                    .font(.custom("Raffina", size: sizeFond / 6))
+                    .foregroundColor(Color(.green))
+                Text("2020")
+                    .foregroundColor(.gray)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Text("calculator antibiotics")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.gray)
+            }
+            .padding()
+            .offset( y: -100)
+            
+            Button(action: { self.isShow.toggle()
+
+
+            }) {
+                VStack{
+
+                    
+                         LottieView(filename: "a567", isPaused: isShow)
+                        .frame(width: sizeFond / 3, height:sizeFond / 3 )
+                        .background(Color
+                            .white
+                            .opacity(0.4)
+                            .frame(width: sizeFond / 2, height: sizeFond / 2)
+                            .cornerRadius(30)
+                            
+                            .rotation3DEffect(.init(degrees: 10), axis: (x: 0, y: 1, z: 0)) )
+                            .shadow(color: Color.black, radius: 9, x: 5, y: 5)
+                        
+                        .padding()
+                    
+                    Text("start")
+                       .font(.system(size:  sizeFond / 10))
+                        .foregroundColor(Color.white)
+                        .shadow(color: Color.black, radius: 5)
+                    .offset( y: -(sizeFond / 10))
+                    }
+
+                }
+
+        }
     }
 }
 
